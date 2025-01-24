94da28231b7d3bda0faed0b3789ba40d19baa762
Sergey Martynenko <martynenko1sv@gmail.com>
2025-01-24 13:54:25 -0500

    Add I/O for external Field Response

--- pixsi/__main__.py
+++ pixsi/__main__.py
@@ -24,11 +24,20 @@ def cli(ctx, store, outstore):
 @cli.command()
 @click.option("-i","--input", type=str, required=False,
               help="Placeholder")
+@click.option("-k","--kernelresp",type=click.Path(),required=False,
+              help="Path to Field Response")
 @click.pass_context
-def run(ctx,input):
+def run(ctx,input,kernelresp):
     '''
     Runs Toy Simulation + Reconstruction 
     '''
+    
+    if not kernelresp:
+        print("Path to FR was not provided. Using Toy Kernel")
+        kernel=pixsi.toy_sim.kernel()
+    else:
+        kernel=pixsi.toy_sim.getKernel(kernelresp)
+    import matplotlib.pyplot as plt
     import numpy as np
     #define example true signal
     time_steps = np.linspace(0, 160, 1600)
@@ -38,16 +47,20 @@ def run(ctx,input):
     true_signal[93:94] = 7.0
     true_signal[94:95] = 2.0# Example true signal
     #true_signal[187:187+4+1] = 1.0
-    kernel=pixsi.toy_sim.kernel()
+    plt.plot(time_steps,kernel)
+    plt.plot(time_steps,np.cumsum(kernel))
+    plt.title("Current Kernel")
+    plt.xlabel("Time (us)")
+    #plt.xlim(0,5)
+    plt.show()
     #Get example induced Current
     current_response = pixsi.toy_sim.compute_current(true_signal, kernel, time_steps)
     
     
-    trsh=200
+    trsh=0.001
     #Get example sparce triggered mesurements (time,value)
     M,q=pixsi.toy_sim.trigger(current_response,trsh)
     b=np.array(M)
-    import matplotlib.pyplot as plt
     plt.figure(figsize=(12, 6))
     plt.plot(time_steps, true_signal*150, label="True Signal (Charge)", linestyle="--")
     plt.plot(time_steps, current_response, label="True Current (Current)")
@@ -75,16 +88,17 @@ def run(ctx,input):
     
     
     #Plot Results
-    print(RecoveredSignals)
+    lk=len(kernel[kernel!=0])
+    print(RecoveredSignals,lk)
     Recovedred_dense=np.zeros(1600)
     t_st=int(RecoveredSignals[0][0])
     Q=RecoveredSignals[0][1:]
-    dt1=M_blocks[0][0][0]+32-t_st
+    dt1=M_blocks[0][0][0]+lk-t_st
     dt2=M_blocks[0][1][0]-M_blocks[0][0][0]
     dt3=M_blocks[0][2][0]-M_blocks[0][1][0]
-    Recovedred_dense[t_st:M_blocks[0][0][0]+32]=Q[0]/dt1
-    Recovedred_dense[M_blocks[0][0][0]+32:M_blocks[0][1][0]+32]=Q[1]/dt2
-    Recovedred_dense[M_blocks[0][1][0]+32:M_blocks[0][2][0]+32]=Q[2]/dt3
+    Recovedred_dense[t_st:M_blocks[0][0][0]+lk]=Q[0]/dt1
+    Recovedred_dense[M_blocks[0][0][0]+lk:M_blocks[0][1][0]+lk]=Q[1]/dt2
+    Recovedred_dense[M_blocks[0][1][0]+lk:M_blocks[0][2][0]+lk]=Q[2]/dt3
     print("Time Intervals: ",dt1,dt2,dt3)
     print("True signal=",np.sum(true_signal[:150]))
     print("Recovered signal=",np.sum(Recovedred_dense[:150]))
@@ -93,17 +107,20 @@ def run(ctx,input):
     plt.plot(time_steps, true_signal*1, label="True Signal (Charge)", linestyle="--")
     
     modified_signal = np.zeros(len(true_signal))
-    dt1_true=58+32-63
+    dt1_true=M_blocks[0][0][0]+lk-63
     dt2_true=16
-    avg1=np.sum(true_signal[63:58+32])/dt1_true
-    avg2=np.sum(true_signal[58+32:58+32+32])/dt2_true
+    dt3_true=28
+    avg1=np.sum(true_signal[63:M_blocks[0][0][0]+lk])/dt1_true
+    avg2=np.sum(true_signal[M_blocks[0][0][0]+lk:M_blocks[0][1][0]+lk])/dt2_true
+    avg3=np.sum(true_signal[M_blocks[0][1][0]+lk:M_blocks[0][2][0]+lk])/dt3_true if dt3_true>0 else 0
     modified_signal[63:63+dt1_true]=avg1
     modified_signal[63+dt1_true:63+dt1_true+dt2_true]=avg2
-    
+    modified_signal[63+dt1_true+dt2_true:63+dt1_true+dt2_true+dt3_true]=avg3
+    print("Expectation: ",np.array([63,avg1*dt1_true,avg2*dt2_true,avg3*dt3_true]))
     plt.plot(time_steps, modified_signal, label="True Signal (Modified)", linestyle="--")
     plt.legend()
     plt.plot()
-    plt.xlim(6,11)
+    plt.xlim(4,13)
     plt.ylim(0,1.5)
     plt.show()
 
--- pixsi/kernels.py
+++ pixsi/kernels.py
@@ -1,4 +1,22 @@
 import numpy as np
+from scipy.interpolate import interp1d
+
+def getKernel(path):
+    #currently supports specific format. Needs generalization
+    current = np.load(path)['pixel'][191-3]
+    
+    time_05 = np.arange(0, len(current) * 0.05, 0.05)[:-1]
+    time_1 = np.arange(0, len(current) * 0.05, 0.1)
+    print(len(current),len(time_05),len(time_1))
+    interpolator = interp1d(time_05, current, kind='linear')
+    current_sampled = interpolator(time_1)#[:1600]
+    #modification to fit current simulation constraints
+    nonzeroIdxes = np.nonzero(current_sampled)[0]
+    res=np.zeros(1600)
+    res[:60]=current_sampled[nonzeroIdxes[-1]-60:nonzeroIdxes[-1]]
+    return res
+    #nonzeroIdxes = np.nonzero(current_sampled)[0]
+
 
 def kernel_3us():
     # For 3 us kernel sigma is 1.5 and drft is 5/speed
@@ -12,6 +30,21 @@ def kernel_3us():
         return peak_value * np.exp(t**2 / sigma**2) if t <= drift_time else 0
 
     return np.array([half_gaussian(t, sigma, peak_value, drift_time) for t in time_steps])
+
+def kernel_1us():
+    #1us kernel
+    drift_speed = 1.6
+    sigma = 0.5
+    peak_value = 1.0
+    drift_time = 2.0 / drift_speed
+    time_steps = np.linspace(0, 160, 1600)
+
+    def half_gaussian(t, sigma, peak_value, drift_time):
+        return peak_value * np.exp(t**2 / sigma**2) if t <= drift_time else 0
+
+    return np.array([half_gaussian(t, sigma, peak_value, drift_time) for t in time_steps])
+
+
 
 def kernel():
     #Default 2us kernel
